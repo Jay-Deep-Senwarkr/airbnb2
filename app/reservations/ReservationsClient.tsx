2d78d5ca9f7dc50ac7bc34510a61260e22dca87e
Jay-Deep-Senwark <jaydeepsenwarkr@gmail.com>
2024-04-21 22:20:06 +0530

    third

---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,22 @@
+'ues client'
+import {toast} from "react-hot-toast";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { SafeReservation, SafeUser } from "../types";
+import Heading from "../components/Heading";
+import Container from "../components/Container";
+import ListingCard from "../components/listings/ListingCard";
+
+const ReservationsClient = () => {
+    return ( <div>
+        <Container>
+            <Heading 
+            title="Reservations"
+            subtitle="Bookings on your properties"
+            />
+        </Container>
+    </div> );
+}
+ 
+export default ReservationsClient;
