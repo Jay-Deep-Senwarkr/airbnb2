f94ddcc4ac48dfa88721f794ce89c6a6d37174b1
Jay-Deep-Senwark <jaydeepsenwarkr@gmail.com>
2024-04-26 02:18:24 +0530

    last push

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/Modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/Modals/RentModal";
+import SearchModal from "./components/Modals/SearchModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,14 +32,13 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <RentModal/>
+          <SearchModal />
+          <RentModal />
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className="pb-20 pt-28">
-        {children}
-        </div>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
